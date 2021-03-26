e9039c0f088f67572d64e72798ff0534efd458ac
ShaunMundy <80899830+ShaunMundy@users.noreply.github.com>
2021-03-26 11:45:33 +0100

    Update upd-phpmyadmn.sh

--- upd-phpmyadmn.sh
+++ upd-phpmyadmn.sh
@@ -2,7 +2,7 @@
 mv /usr/share/phpmyadmin/ /usr/share/phpmyadmin.bak
 mkdir /usr/share/phpmyadmin/
 cd /usr/share/phpmyadmin/
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar xzf phpMyAdmin-5.0.2-all-languages.tar.gz
 mv phpMyAdmin-5.0.2-all-languages/* /usr/share/phpmyadmin
 sed -i "s#('TEMP_DIR', './tmp/')#('TEMP_DIR', '/var/lib/phpmyadmin/tmp/')#g" /usr/share/phpmyadmin/libraries/vendor_config.php
