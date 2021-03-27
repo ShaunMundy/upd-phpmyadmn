e0d1af83a6f6cdf725c09052ccc635462d52476b
ShaunMundy <80899830+ShaunMundy@users.noreply.github.com>
2021-03-27 14:15:16 +0100

    Update upd-phpmyadmn.sh

--- upd-phpmyadmn.sh
+++ upd-phpmyadmn.sh
@@ -2,11 +2,11 @@
 mv /usr/share/phpmyadmin/ /usr/share/phpmyadmin.bak
 mkdir /usr/share/phpmyadmin/
 cd /usr/share/phpmyadmin/
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
-tar xzf phpMyAdmin-5.0.2-all-languages.tar.gz
-mv phpMyAdmin-5.0.2-all-languages/* /usr/share/phpmyadmin
-sed -i "s#('TEMP_DIR', './tmp/')#('TEMP_DIR', '/var/lib/phpmyadmin/tmp/')#g" /usr/share/phpmyadmin/libraries/vendor_config.php
-sed -i "s#('CONFIG_DIR', '')#('CONFIG_DIR', '/etc/phpmyadmin/')#g" /usr/share/phpmyadmin/libraries/vendor_config.php
-rm /usr/share/phpmyadmin/phpMyAdmin-5.0.2-all-languages.tar.gz
-rm -rf /usr/share/phpmyadmin/phpMyAdmin-5.0.2-all-languages
+wget https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.tar.gz
+tar xzf phpMyAdmin-5.1.0-all-languages.tar.gz
+mv phpMyAdmin-5.1.0-all-languages/* /usr/share/phpmyadmin
+sed -i "s#('TEMP_DIR', ROOT_PATH . 'tmp' . DIRECTORY_SEPARATOR)#('TEMP_DIR', '/var/lib/phpmyadmin/tmp/')#g" /usr/share/phpmyadmin/libraries/vendor_config.php
+sed -i "s#('CONFIG_DIR', ROOT_PATH)#('CONFIG_DIR', '/etc/phpmyadmin/')#g" /usr/share/phpmyadmin/libraries/vendor_config.php
+rm /usr/share/phpmyadmin/phpMyAdmin-5.1.0-all-languages.tar.gz
+rm -rf /usr/share/phpmyadmin/phpMyAdmin-5.1.0-all-languages
 rm -rf /usr/share/phpmyadmin.bak
